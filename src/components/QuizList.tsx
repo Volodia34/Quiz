ff6a570c8c1fff72ca1c38f646081f8e5fc9f99a
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2024-05-24 08:14:36 +0300

    add QuizList and Routes

---
+++ src/components/QuizList.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getQuizzes, deleteQuiz } from '../utils/localStorage';
+
+const QuizList: React.FC = () => {
+    const [quizzes, setQuizzes] = useState<any[]>([]);
+
+    useEffect(() => {
+        setQuizzes(getQuizzes());
+    }, []);
+
+    const handleDelete = (id: number) => {
+        deleteQuiz(id);
+        setQuizzes(getQuizzes());
+    };
+
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">Quiz List</h1>
+            <Link to="/add" className="btn btn-primary mb-4">Add Quiz</Link>
+            <ul>
+                {quizzes.map(quiz => (
+                    <li key={quiz.id} className="flex justify-between items-center my-2 p-2 border rounded">
+                        <span className="flex-1">{quiz.name}</span>
+                        <Link to={`/edit/${quiz.id}`} className="mx-2 text-blue-500">Edit</Link>
+                        <Link to={`/take/${quiz.id}`} className="mx-2 text-green-500">Take Quiz</Link>
+                        <button
+                            onClick={() => handleDelete(quiz.id)}
+                            className="ml-2 text-red-500"
+                        >
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default QuizList;
---
+++ src/components/QuizTaker.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuizTaker = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default QuizTaker;
