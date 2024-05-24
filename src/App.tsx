0ef2dfb6f16436e413c34f73a4351595d8dbed71
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2024-05-24 17:05:16 +0300

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import QuizList from "./components/QuizList";
 import QuizForm from "./components/QuizForm";
